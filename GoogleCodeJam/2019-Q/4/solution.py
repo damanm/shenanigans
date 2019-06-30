2d29f48ca9e0660933296993398832510701be84
damanm <m.damanmohan@gmail.com>
2019-06-30 13:04:37 +0530

    pushing latest changes from personal laptop

--- GoogleCodeJam/2019-Q/4/solution.py
+++ GoogleCodeJam/2019-Q/4/solution.py
@@ -31,40 +31,80 @@ def query_system(test_string):
 	print(test_string, flush=True)
 	return sys.stdin.readline().rstrip()
 
-def find_no_broken_machines(response, start_index, end_index, char):
+def find_no_broken_machines(response, start_index, repeats, char):
+	print("start: %d | repeats: %d" % (start_index, repeats))
 	no_broken_machines = 0
-	for index in range(start_index, end_index+1):
-		if char != response[index]:
-			no_broken_machines = end_index-index+1
+	count = 0
+	index = start_index
+	while(index<len(response) and count < repeats):
+		if char == response[index]:
+			count+=1
+		else:
 			break
+		index+=1
+	no_broken_machines = repeats-count
+#	for index in range(start_index, end_index+1):
+#		if char != response[index]:
+#			no_broken_machines = end_index-index+1
+#			break
 	return no_broken_machines
 
-def get_broken_machine_map(response, repeats):
+def get_broken_machine_map(response, no_broken_machines):
 	chars = ['0', '1']
 	to_find = 0
-	count = 0
+	found_broken_machines = 0
 	map = []
 	index = 0
-	while(index<len(response)):
-		end_index = repeats+index-1 if (repeats+index-1) < len(response) else len(response) - 1
-		no_broken = find_no_broken_machines(response, index, end_index, chars[to_find])
-		map.append([end_index, no_broken])
+	repeats = get_bit_repeats(no_broken_machines)
+	while(found_broken_machines < no_broken_machines):
+#		end_index = repeats+index-1 if (repeats+index-1) < len(response) else len(response) - 1
+		# calculate indices of sector of bits in original test_string from where bits are missing
+		sector_start = index + found_broken_machines # accounting for the missing bits by adding already identified broken machines
+		sector_end = sector_start+repeats-1
+		
+		# find number of broken bits in sector
+		no_broken = find_no_broken_machines(response, index, repeats, chars[to_find])
+		
+		# add info to map
+		map.append([sector_start, sector_end, no_broken])
+		
+		# update count of found broken machines
+		found_broken_machines += no_broken
+		
+		# update character to look for
 		to_find=to_find^1
-		index = end_index+1-no_broken
+		
+		# update index to start of next sector in response string
+		index = index+repeats-no_broken
 	return map
 
-def find_broken_machines(no_bits, no_broken_machines):
+def find_broken_machines(total_no_bits, no_broken_machines):
 	# get repeats for number of broken machines
 	repeats = get_bit_repeats(no_broken_machines)
 	
 	# generate test string to query the system
-	test_string = generate_bit_string(no_bits, repeats)
+	test_string = generate_bit_string(total_no_bits, repeats)
 	
 	# query the system
 	response = query_system(test_string)
 	
 	# identify number of broken machines in each section of test_string
-	bit_trans = get_bit_transition_indices(response)
+	broken_machine_map = get_broken_machine_map(response, no_broken_machines)
+	
+	broken_machines = []
+	
+	while(True):
+		for entry in map:
+			# bit has been tracked down
+			if entry[0] == entry[1]:
+				broken_machines.append(entry[0])
+			# entire bit sequence missing in response -> multiple bits tracked!
+			elsif: entry[1]-entry[0] == entry[2]
+				for i in range(entry[0],entry[1]+1):
+					broken_machines.append(i)
+		if len(broken_machines) == no_broken_machines;
+			break
+		# there are broken machines to be found
 
 def main():
 	cases = int(sys.stdin.readline().rstrip())
@@ -83,5 +123,12 @@ if __name__ == "__main__":
 #	main()
 #	for i in range(1,16):
 #		print("%d %d" % (i, get_bit_repeats(i))
-	output = get_broken_machine_map("001101", 2)
+	output = find_broken_machines(8, 2)
+#	output = find_no_broken_machines("001100", 0, 2, '0')
+#	print(output)
+#	output = find_no_broken_machines("001100", 2, 2, '1')
+#	print(output)
+#	output = find_no_broken_machines("001100", 4, 2, '0')
+#	print(output)
+#	output = find_no_broken_machines("001100", 6, 2, '1')
 	print(output)
